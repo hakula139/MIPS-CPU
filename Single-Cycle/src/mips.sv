0fb59803ba2051d2523847c4a4af63cd466c5c72
Hakula Chen <i@hakula.xyz>
2020-03-16 02:28:42 +0800

    Implement mips

---
+++ Single-Cycle/src/mips.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+
+// Single-cycle 32-bit MIPS processor
+module mips (
+  input               clk,
+  input               reset,
+  input        [31:0] instr,
+  input        [31:0] readdata,
+  output logic [31:0] pc,
+  output logic        memwrite,
+  output logic [31:0] aluout,
+  output logic [31:0] writedata
+);
+
+  logic mem_to_reg, branch, pc_src, zero, alu_src, reg_dst, reg_write, jump;
+  logic [3:0] alu_control;
+
+  control_unit u_control_unit (
+    .op_i(instr[31:26]),
+    .funct_i(instr[5:0]),
+    .zero_i(zero),
+    .mem_to_reg_o(mem_to_reg),
+    .mem_write_o(memwrite),
+    .pc_src_o(pc_src),
+    .jump_o(jump),
+    .alu_control_o(alu_control),
+    .alu_src_o(alu_src),
+    .reg_dst_o(reg_dst),
+    .reg_write_o(reg_write)
+  );
+
+  datapath     u_datapath (
+    .clk_i(clk),
+    .rst_i(reset),
+    .instr_i(instr),
+    .mem_to_reg_i(mem_to_reg),
+    .pc_src_i(pc_src),
+    .jump_i(jump),
+    .alu_control_i(alu_control),
+    .alu_src_i(alu_src),
+    .reg_dst_i(reg_dst),
+    .reg_write_i(reg_write),
+    .read_data_i(readdata),
+    .pc_o(pc),
+    .alu_result_o(aluout),
+    .zero_o(zero),
+    .write_data_o(writedata)
+  );
+
+endmodule : mips
